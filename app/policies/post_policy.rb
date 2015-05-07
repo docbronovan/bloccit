1432f8c2a09a254dc66fee2b19156d5da1742375
docbronovan <brockdonovan@gmail.com>
2015-05-07 08:47:48 -0500

    updated user.rb and post_policy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,27 @@
 class PostPolicy < ApplicationPolicy
+    class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? OR user.moderator?
+        scope.all
+      elsif user.member?
+        scope.where(:published => true)
+      else
+        scope.none
+      end
+    end
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+
   def index?
     true
   end
