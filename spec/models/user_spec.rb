7697db4053e167bcae04ad4a39005dd705146565
docbronovan <brockdonovan@gmail.com>
2015-05-27 09:29:28 -0500

    assignment voting, second and third test fail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
-descibe User do 
+describe User do 
   
   include TestFactories
 
   describe "#favorited(post)" do
-    xit "returns `ni` if the user has not favorited the post" do
+    #creating common objects in a before block
+    before do
+      @post = associated_post
+      @user = authenticated_user
+      @favorite = @user.favorited(@post) 
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@favorite).to eq nil
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      @user.favorites.build(post: @post)
+      expect( @user.favorited(@post) ).to eq true
+    end
+
+    #confirm the favorited method returns nil if the user has favorited a different post than the post being passed into favorited
+    it "returns `nil` if the user has favorited another post" do
     end
   end
 
