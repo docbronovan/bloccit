f83cea100874b6d359e84713173ec3886af50852
docbronovan <brockdonovan@gmail.com>
2015-06-03 19:51:35 -0500

    testing users complete

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,24 @@
 FactoryGirl.define do
- factory :user do
-   name "Douglas Adams"
-   sequence(:email, 100) { |n| "person#{n}@example.com" }
-   password "helloworld"
-   password_confirmation "helloworld"
-   confirmed_at Time.now
- end
+  factory :user do
+    name "Douglas Adams"
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password "helloworld"
+    password_confirmation "helloworld"
+    confirmed_at Time.now
+ 
+    factory :user_with_post_and_comment do
+      # @user1 = create(:user)
+      # post = create(:post, user: @user1)
+      # create(:comment, user: @user1, post: post)
+
+      # @user2 = create(:user)
+      # post = create(:post, user: @user2)
+      # 2.times { create(:comment, user: @user2, post: post)}
+      after :build do |user|
+        post = create(:post, user: user)
+        create(:comment, user: user, post: post)
+      end
+    end
+    
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,14 +14,14 @@ describe User do
 
   describe ".top_rated" do
     before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
+      @user1 = create(:user_with_post_and_comment)
+   #    post = create(:post, user: @user1)
+   #    create(:comment, user: @user1, post: post)
 
-      @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post)}
-    end
+       @user2 = create(:user)
+       post = create(:post, user: @user2)
+       2.times { create(:comment, user: @user2, post: post)}
+     end
 
     it "returns users ordered by comments + posts" do
       expect( User.top_rated ).to eq([@user2, @user1])
